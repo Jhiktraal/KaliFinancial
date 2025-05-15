6c82ef769488eaeea521741c63c9bf038264aeb5
Jhiktraal <ema.gon.1015@gmail.com>
2025-05-14 21:45:48 -0300

    Delete backend_respaldo directory

--- backend_respaldo/convertir_a_excel.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Leer el CSV
-df = pd.read_csv('ejemplo_importacion.csv')
-
-# Guardar como Excel
-df.to_excel('ejemplo_importacion.xlsx', index=False)
-print("Archivo Excel creado exitosamente!") 
